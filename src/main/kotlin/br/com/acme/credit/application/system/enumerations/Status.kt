ad6bb549f74d1752161384f15f17799c86a663dd
ogQoda <carlos_ojr@live.com>
2023-05-26 17:25:17 -0300

    Entities creation, h2 first configuration and annotations added

---
+++ src/main/kotlin/br/com/acme/credit/application/system/enumerations/Status.kt
@@ -0,0 +1,7 @@
+package br.com.acme.credit.application.system.enumerations
+
+enum class Status {
+    IN_PROGRESS,
+    APPROVED,
+    REJECTED
+}
---
+++ src/main/kotlin/br/com/acme/credit/application/system/model/Credit.kt
@@ -0,0 +1,34 @@
+package br.com.acme.credit.application.system.model
+
+import br.com.acme.credit.application.system.enumerations.Status
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.*
+
+@Entity
+@Table(name = "tb_credit")
+data class Credit(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long? = null,
+
+        @Column(nullable = false, unique = true)
+        val creditCode: UUID = UUID.randomUUID(),
+
+        @Column(nullable = false)
+        val creditValue: BigDecimal = BigDecimal.ZERO,
+
+        @Column(nullable = false)
+        val dayFirstInstallment: LocalDate,
+
+        @Column(nullable = false)
+        val numberOfInstallment: Int = 0,
+
+        @Enumerated
+        val status: Status = Status.IN_PROGRESS,
+
+        @ManyToOne
+        val customer: Customer? = null
+
+)
